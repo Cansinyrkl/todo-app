3b1dd3fdbc03e7a5c31614a24f355321190cf8e3
Can <ayurekl97@gmail.com>
2023-03-01 11:48:56 +0300

    error fix & login validate

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -6,11 +6,11 @@ import {
 } from "../styledComponents/StyledComponents";
 import { UserListContext } from "../../store/context/useUserContext";
 import { Formik } from "formik";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { users } = useContext(UserListContext);
-  const navigate = useContext(Navigate);
+  const navigate = useNavigate();
 
   return (
     <Container>
@@ -19,18 +19,59 @@ const Login = () => {
         validate={(values) => {
           const errors = {};
           if (!values.username) {
-            errors.username = "please enter a username";
+            errors.username = "username boş bırakılamaz";
           }
           if (!values.password) {
-            errors.password = "please enter a password";
+            errors.password = "password boş bırakılamaz";
           } else if (
             /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
           ) {
-            errors.username = "undefined symbol  ";
+            errors.username = "geçersiz username ";
           }
           return errors;
         }}
-      ></Formik>
+        onSubmit={(values) => {
+          const checkUser = users.find(
+            ({ name, password }) =>
+              values.username === name && values.password === password
+          );
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <FormContainer>
+            <form onSubmit={handleSubmit}>
+              <h1>UserName</h1>
+              <input
+                className="username"
+                type="text"
+                name="username"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+              />
+              {errors.username && touched.username && errors.username}
+              <h1>Password</h1>
+              <input
+                className="password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              {errors.password && touched.password && errors.password}
+              <GeneralButton type="submit">Submit</GeneralButton>
+            </form>
+          </FormContainer>
+        )}
+      </Formik>
     </Container>
   );
 };
