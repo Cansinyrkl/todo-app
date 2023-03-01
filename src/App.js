4fd017d528651113c705843ff0bb48c9db91af1c
Can <ayurekl97@gmail.com>
2023-03-01 11:47:51 +0300

    Routes wrap Login & Todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Login from "./components/login/Login";
+import Todo from "./components/Todo/Todo";
 import UserListContextProvider from "./store/context/useUserContext";
 import TodoListContextProvider from "./store/context/useTodoList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <UserListContextProvider>
+      <UserListContextProvider> 
         <TodoListContextProvider>
-          <Login />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/todo" element={<Todo />} />
+          </Routes>
         </TodoListContextProvider>
       </UserListContextProvider>
     </div>
