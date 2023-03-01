fca2ac879eaaf54dd028b5f3d0651bd1777cc1b5
Can <ayurekl97@gmail.com>
2023-03-01 10:04:27 +0300

    styledcomponents buttons

---
+++ src/components/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GeneralButton } from "../styledComponents/StyledComponents";
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <GeneralButton>asdasd</GeneralButton>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/styledComponents/StyledComponents.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GeneralButton = styled.button`
+  background-color: #0099ff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #0066ff;
+  }
+`;
