c959f36a10a49bdb6313c13c2088377d05b34777
Can <ayurekl97@gmail.com>
2023-03-01 11:06:10 +0300

    user & todo app-context-wrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Login from "./components/login/Login";
+import UserListContextProvider from "./store/context/useUserContext";
+import TodoListContextProvider from "./store/context/useTodoList";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <UserListContextProvider>
+        <TodoListContextProvider>
+          <Login />
+        </TodoListContextProvider>
+      </UserListContextProvider>
     </div>
   );
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GeneralButton } from "../styledComponents/StyledComponents";
+import { UserListContext } from "../../store/context/useUserContext";
 
 const Login = () => {
+  const { users } = useContext(UserListContext);
+
   return (
     <>
       <div>
