855be62893c61dcf5dc7ce18b3a22bbd28d6dc7f
Can <ayurekl97@gmail.com>
2023-03-01 17:49:56 +0300

    Arrangement components added

---
+++ src/components/arrangement/Arrangement.js
@@ -0,0 +1,61 @@
+import Form from "react-bootstrap/Form";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { useUserList } from "../../hooks/TodoListHooks";
+import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Arrangement({ productHeader, selectedId }) {
+  const [show, setShow] = useState(false);
+  const [changeName, setChangeName] = useState(null);
+
+  const { todoDispatch } = useUserList();
+  const handleEdit = () => {
+    setShow(false);
+    todoDispatch({
+      type: "EDIT_TODO",
+      selectedId: selectedId,
+      newName: changeName,
+    });
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        <FontAwesomeIcon icon={faPenSquare} />
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{productHeader}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                placeholder="Enter a different to do."
+                autoFocus
+                onChange={(e) => setChangeName(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleEdit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Arrangement;
