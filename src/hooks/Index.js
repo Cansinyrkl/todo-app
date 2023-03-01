58340093154255e1c1e6e8e1d9db42feac8d5058
Can <ayurekl97@gmail.com>
2023-03-01 17:48:48 +0300

    hooks added userListContext

--- src/hooks/Index.js
+++ src/hooks/Index.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
 import { TodoListContext } from "../store/context/useTodoList";
-import { useUserContext } from "../store/context/useUserContext";
+import { UserListContext } from "../store/context/useUserContext";
 
-export { useContext, TodoListContext, useUserContext };
+export { useContext, TodoListContext, UserListContext };
