901f2cbc5037830d0d3d184a7c692864e1bba481
Can <ayurekl97@gmail.com>
2023-03-01 11:57:34 +0300

    useTodoList , Todo component defined

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { TodoListContext } from "../../store/context/useTodoList";
 
 const Todo = () => {
+  const [todoAdd, setTodoAdd] = useState("");
+  const todoList = useContext(TodoListContext);
+  const list = todoList.todo;
+  const todoDispatch = todoList.todoDispatch;
+console.log(list)
   return <div>Todo</div>;
 };
 
