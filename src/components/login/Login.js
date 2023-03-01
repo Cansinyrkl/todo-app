aed6a081fea4945e0bcd073583ac66dcbc6f9f40
Can <ayurekl97@gmail.com>
2023-03-01 11:31:42 +0300

    login-errror-validate

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,16 +1,37 @@
 import React, { useContext } from "react";
-import { GeneralButton } from "../styledComponents/StyledComponents";
+import {
+  GeneralButton,
+  Container,
+  FormContainer,
+} from "../styledComponents/StyledComponents";
 import { UserListContext } from "../../store/context/useUserContext";
+import { Formik } from "formik";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
   const { users } = useContext(UserListContext);
+  const navigate = useContext(Navigate);
 
   return (
-    <>
-      <div>
-        <GeneralButton>asdasd</GeneralButton>
-      </div>
-    </>
+    <Container>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.username) {
+            errors.username = "please enter a username";
+          }
+          if (!values.password) {
+            errors.password = "please enter a password";
+          } else if (
+            /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
+          ) {
+            errors.username = "undefined symbol  ";
+          }
+          return errors;
+        }}
+      ></Formik>
+    </Container>
   );
 };
 
