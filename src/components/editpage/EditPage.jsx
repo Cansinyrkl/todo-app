ab62bce77321fff5cc28b81f0ddcc8d6b6729c9d
Can <ayurekl97@gmail.com>
2023-03-02 21:56:38 +0300

    popup style and message timeout added.

--- src/components/editpage/EditPage.jsx
+++ src/components/editpage/EditPage.jsx
@@ -1,14 +1,15 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useTodoList } from "../../hooks/useTodoList";
 import "../.././App.css";
 import Popup from "../../popup/Popup";
+import LogOut from "../logout/LogOut.jsx";
 
 const EditPage = () => {
   const [changeName, setChangeName] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
   let { id } = useParams();
   const navigate = useNavigate();
-
   const { todoDispatch, todo } = useTodoList();
 
   const filterData = useMemo(() => {
@@ -21,20 +22,31 @@ const EditPage = () => {
       selectedId: filterData.id,
       newName: changeName,
     });
-    navigate("/todo");
+    setShowPopup(true);
+    setTimeout(() => navigate("/todo"), 3000);
+    setTimeout(() => setShowPopup(false), 2000);
   };
 
   return (
     <div className="edit-container">
-      <input
-        className="task-input"
-        type="text"
-        placeholder="Enter a different to do."
-        defaultValue={filterData.name}
-        onChange={(e) => setChangeName(e.target.value)}
-        autoFocus
-      />
-      <button className="submit-task" onClick={handleEdit} />
+      {showPopup && <Popup />}
+      <LogOut />
+      <h1 className="edit-header">TO DO DEDIT</h1>
+      <div className="edit-task">
+        <input
+          className="task-input"
+          type="text"
+          placeholder="Enter a different to do."
+          defaultValue={filterData.name}
+          onChange={(e) => setChangeName(e.target.value)}
+          autoFocus
+        />
+        <button
+          className="submit-task"
+          onClick={handleEdit}
+          disabled={!changeName}
+        />
+      </div>
     </div>
   );
 };
