cdb710bb932cb529cab878fc61ef21203af49f6e
Can <ayurekl97@gmail.com>
2023-03-02 21:14:27 +0300

    login page ui desing changed.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -10,76 +10,77 @@ const Login = () => {
 
   return (
     <div class="flex-container">
-      <div class="content-container">
-        <div class="form-container">
-          <Formik
-            initialValues={{ username: "", password: "" }}
-            validate={(values) => {
-              const errors = {};
-              if (!values.username) {
-                errors.username = "username cannot be left blank";
-              }
-              if (!values.password) {
-                errors.password = "password cannot be left blank";
-              } else if (
-                /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
-              ) {
-                errors.username = "geçersiz username ";
-              }
-              return errors;
-            }}
-            onSubmit={(values) => {
-              const checkUser = users.find(
-                ({ name, password }) =>
-                  values.username === name && values.password === password
-              );
-              if (checkUser) {
-                navigate("/todo");
-                sessionStorage.setItem(
-                  "userInfo",
-                  JSON.stringify(checkUser.id)
-                );
-              }
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-            }) => (
-              <div className="LoginContainer">
-                <form onSubmit={handleSubmit}>
-                  <h1>UserName</h1>
-                  <input
-                    className="username"
-                    type="text"
-                    name="username"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.username}
-                  />
+      <div class="form-container">
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.username) {
+              errors.username = "username cannot be left blank";
+            }
+            if (!values.password) {
+              errors.password = "password cannot be left blank";
+            } else if (
+              /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
+            ) {
+              errors.username = "geçersiz username ";
+            }
+            return errors;
+          }}
+          onSubmit={(values) => {
+            const checkUser = users.find(
+              ({ name, password }) =>
+                values.username === name && values.password === password
+            );
+            if (checkUser) {
+              navigate("/todo");
+              sessionStorage.setItem("userInfo", JSON.stringify(checkUser.id));
+            }
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit} className="LoginContainer">
+              <h2>User Name</h2>
+              <div className="input-group">
+                <input
+                  className="username"
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.username}
+                />
+                <div className="error-style">
                   {errors.username && touched.username && errors.username}
-                  <h1>Password</h1>
-                  <input
-                    className="password"
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                  />
-                  {errors.password && touched.password && errors.password}
-                  <button type="submit" class="submit-btn">
-                    Login
-                  </button>
-                </form>
+                </div>
               </div>
-            )}
-          </Formik>
-        </div>
+              <h2>Password</h2>
+              <div className="input-group">
+                <input
+                  className="password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <div className="error-style">
+                  {errors.password && touched.password && errors.password}
+                </div>
+              </div>
+              <button type="submit" class="submit-btn">
+                Login
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
